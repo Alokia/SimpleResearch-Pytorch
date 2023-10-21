17acae3f43328fbda17af79bd9815c19e3df6744
Eloik <443345680@qq.com>
2023-10-22 00:01:20 +0800

    add DepthwiseSeparableConvolution

---
+++ conv/DepthwiseSeparableConvolution.py
@@ -0,0 +1,44 @@
+"""
+Chollet F. Xception: Deep learning with depthwise separable convolutions[C]//
+Proceedings of the IEEE conference on computer vision and pattern recognition. 2017: 1251-1258.
+"""
+import torch
+from torch import nn
+
+
+class DepthwiseSeparableConvolution(nn.Module):
+    """
+    Depthwise Separable Convolution
+
+    相比于普通卷积，深度可分离卷积的参数量更小，可以搭建更深的模型。
+    """
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        super().__init__()
+        self.depthwise_conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            groups=in_channels
+        )
+        self.pointwise_conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            groups=1
+        )
+
+    def forward(self, x):
+        out = self.depthwise_conv(x)
+        out = self.pointwise_conv(out)
+        return out
+
+
+if __name__ == "__main__":
+    x = torch.randn(2, 3, 224, 224)
+    dsconv = DepthwiseSeparableConvolution(3, 64, kernel_size=3, stride=2)
+    out = dsconv(x)
+    print(out.shape)
