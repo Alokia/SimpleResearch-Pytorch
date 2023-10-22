ca80e00148bc385f57844ac232dcb9295cf3c8d4
Eloik <443345680@qq.com>
2023-10-22 22:30:18 +0800

    update MultiHeadAttention params

--- attention/SelfAttention.py
+++ attention/SelfAttention.py
@@ -81,14 +81,7 @@ class MultiHeadAttention(nn.Module):
 
     def init_weights(self):
         for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
+            if isinstance(m, nn.Linear):
                 nn.init.normal_(m.weight, std=0.001)
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
