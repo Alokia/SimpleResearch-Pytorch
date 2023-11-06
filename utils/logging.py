484560357e2f2c5afa1d11758522b892bf2f7bd8
Eloik <443345680@qq.com>
2023-11-06 13:16:16 +0800

    add logging

---
+++ utils/logging.py
@@ -0,0 +1,10 @@
+import logging
+
+
+def save_training_log(log_freq, step, batch_size, prefix: str = '', **kwargs):
+    if log_freq is None:
+        return
+    elif log_freq == 0 and step == batch_size - 1:
+        pass
+    elif step % log_freq == 0:
+        pass
