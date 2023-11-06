f79875d6e34d234c8581d7825577ac69811bb4ae
Eloik <443345680@qq.com>
2023-11-06 14:19:48 +0800

    fix a error

--- utils/metrics.py
+++ utils/metrics.py
@@ -23,7 +23,7 @@ def accuracy(y_pred: Tensor, y_true: Tensor, topk: Tuple[int, ...] = (1,)):
 
     res = []
     for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
         res.append(correct_k.mul_(100.0 / batch_size))
 
     return res
