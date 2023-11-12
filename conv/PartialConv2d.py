11590e223a97af5c4aa456832324dc5532e9cf26
Eloik <443345680@qq.com>
2023-11-12 17:20:24 +0800

    add PartialConv2d

---
+++ conv/PartialConv2d.py
@@ -0,0 +1,14 @@
+"""
+Chen J, Kao S, He H, et al. Run, Don't Walk: Chasing Higher FLOPS for Faster Neural Networks[C]//
+Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition. 2023: 12021-12031.
+
+https://arxiv.org/pdf/2303.03667.pdf
+"""
+import torch
+from torch import nn
+
+
+class PConv(nn.Module):
+    pass
+
+
