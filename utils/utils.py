989ccfbf21ea63b47935d96e49a6f5d0ff737010
Eloik <443345680@qq.com>
2023-11-13 23:18:33 +0800

    del seed_everything and Meter

--- utils/utils.py
+++ utils/utils.py
@@ -1,33 +1,6 @@
-import random
-import torch
-import torch.backends.cudnn as cudnn
+from omegaconf import OmegaConf
 
 
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-
-    def __init__(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-
-def seed_everything(seed: int = None):
-    # 固定随机种子，保证结果可复现
-    if seed is not None:
-        random.seed(seed)
-        torch.manual_seed(seed)
-        cudnn.deterministic = True
+# 从yaml文件中读取配置，返回OmegaConf对象
+def load_yaml_with_omegaconf(path: str):
+    return OmegaConf.load(path)
